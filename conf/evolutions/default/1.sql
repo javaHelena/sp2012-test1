03889bfd366afe23c9f98ae4e4b53ef06fa62e55
Helena Hjertén <helena@hjerten.com>
2012-03-14 17:20:13 +0100

    Database file?

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table comment (
+  id                        bigint not null,
+  greeting                  varchar(255),
+  author                    varchar(255),
+  constraint pk_comment primary key (id))
+;
+
+create sequence comment_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists comment;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists comment_seq;
+
